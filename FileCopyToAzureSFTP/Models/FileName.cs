1f8cae439ed610da679a918daeb265b74be6b339
kyashankar <kyashankar@yahoo.com>
2024-11-29 22:58:35 +0530

    Changed the output JSon format for the Files

---
+++ FileCopyToAzureSFTP/Models/FileName.cs
@@ -0,0 +1,9 @@
+﻿namespace FileCopyToAzureSFTP.WebAPI.Models
+{
+    public class FileName
+    {
+        public string FName { get; set; }
+        public  string FType { get; set; }
+        public  string contentType { get; set; }
+    }
+}
